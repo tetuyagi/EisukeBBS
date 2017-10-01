f816dc0600779b7e19d2841f534a8a6a6575d5f9
tetuyagi <tetuyagi@hotmail.com>
2017-10-01 20:42:54 +0900

    changed attr

--- MysqlController.php
+++ MysqlController.php
@@ -20,10 +20,13 @@ $charset = 'utf8';
 $dsn = 'mysql:host='.$host.';dbname='.$dbname.';charset='.$charset;
 $username = 'tyagiserver_root';
 $password = 'password';
-
+$driver_optioins = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES => false
+);
 
 try{
-  $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_EMULATE_PREPARES => false));
+  $pdo = new PDO($dsn, $user, $password, $driver_optioins);
   echo '<p> データベース接続成功。</p>';
 }catch(PDOException $e){
 $message =  'データベース接続失敗。' .$e->getMessage();
